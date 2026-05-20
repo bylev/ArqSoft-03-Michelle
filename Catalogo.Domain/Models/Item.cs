77fab3754bcf0c6146036642e733d241daa8cb5d
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-05-20 08:30:09 -0600

    CREATE: Presentation - Item.json, CatalogoController, Views, Errors

--- Catalogo.Domain/Models/Item.cs
+++ Catalogo.Domain/Models/Item.cs
@@ -7,10 +7,11 @@ namespace CatalogoApp.Domain.Models
     public class Item
     {
         public int Id { get; set; }
-        public string Titulo { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
         public string Genero { get; set; } = string.Empty;
-        public string Consola { get; set; } = string.Empty;
         public int Ano { get; set; }
-        public string Descripcion { get; set; } = string.Empty;
+        public string Formato { get; set; } = string.Empty;
+        public string Artista { get; set; } = string.Empty;
+
     }
 }
--- Catalogo.Presentation/Program.cs
+++ Catalogo.Presentation/Program.cs
@@ -1,19 +1,44 @@
+using CatalogoApp.Application.Services;
+using CatalogoApp.Infrastructure.Repositories;
+using CatalogoApp.Domain.Models;
+using CatalogoApp.Domain.Interfaces;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Ruta del archivo JSON — se guarda en la carpeta "data" del proyecto
+
+var jsonPath = Path.Combine(builder.Environment.ContentRootPath, "Data", "Item.json");
+
+// Registrar el repositorio JSON como implementación ItemRepository
+builder.Services.AddSingleton<IItemRepository>(
+     new JsonItemRepository(jsonPath)
+);
+
+// Registrar el servicio de Application
+builder.Services.AddScoped<ItemService>();
+
+// Registrar autorización
+builder.Services.AddAuthorization();
+
+// Registrar servicios MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
+
 {
+
     app.UseExceptionHandler("/Home/Error");
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); 
 app.UseRouting();
 
 app.UseAuthorization();
@@ -21,8 +46,11 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
+
     name: "default",
+
     pattern: "{controller=Home}/{action=Index}/{id?}")
+
     .WithStaticAssets();
 
 
